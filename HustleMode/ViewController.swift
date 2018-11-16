e97700cca9bb60995b0ad643256c30b15b1ae5e6
Hend Khaled <hend.k.soliman@icloud.com>
2018-11-16 14:57:53 +0200

    Initial Commit

---
+++ HustleMode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HustleMode
+//
+//  Created by Hend Khaled on 11/16/18.
+//  Copyright © 2018 Hend Khaled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
